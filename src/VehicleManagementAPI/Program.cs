16ea8634ad25d53fd3c32fd5c5e857f6a8762678
Gus Theunissen <gus.theunissen142@gmail.com>
2024-10-15 15:04:09 +0200

    #2 added sentry to WebApp.csproj, VehicleManagementAPI.csproj, CustomerManagementAPI.csproj, WorkshopManagementAPI.csproj

--- src/VehicleManagementAPI/Program.cs
+++ src/VehicleManagementAPI/Program.cs
@@ -7,6 +7,30 @@ builder.Host.UseSerilog((context, logContext) =>
         .Enrich.WithMachineName()
 );
 
+builder.WebHost.UseSentry(o =>
+{
+    o.Dsn = builder.Configuration["Sentry:DSN"];
+    // When configuring for the first time, to see what the SDK is doing:
+    o.Debug = true;
+    // Set TracesSampleRate to 1.0 to capture 100%
+    // of transactions for tracing.
+    // We recommend adjusting this value in production
+    o.TracesSampleRate = 1.0;
+    // Sample rate for profiling, applied on top of othe TracesSampleRate,
+    // e.g. 0.2 means we want to profile 20 % of the captured transactions.
+    // We recommend adjusting this value in production.
+    o.ProfilesSampleRate = 1.0;
+    // Requires NuGet package: Sentry.Profiling
+    // Note: By default, the profiler is initialized asynchronously. This can
+    // be tuned by passing a desired initialization timeout to the constructor.
+    o.AddIntegration(new ProfilingIntegration(
+        // During startup, wait up to 500ms to profile the app startup code.
+        // This could make launching the app a bit slower so comment it out if you
+        // prefer profiling to start asynchronously.
+        TimeSpan.FromMilliseconds(500)
+    ));
+});
+
 // add DBContext
 var sqlConnectionString = builder.Configuration.GetConnectionString("VehicleManagementCN");
 builder.Services.AddDbContext<VehicleManagementDBContext>(options => options.UseSqlServer(sqlConnectionString));
