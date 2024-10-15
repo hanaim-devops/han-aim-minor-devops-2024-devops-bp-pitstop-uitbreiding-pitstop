9f60d20b36fb35c4e5dea4080dc812447d61836e
unknown <dirkvbruxvoort@gmail.com>
2024-10-15 20:11:51 +0200

    basic app

--- src/k8s/scripts/start-all.ps1
+++ src/k8s/scripts/start-all.ps1
@@ -56,4 +56,5 @@ kubectl apply `
     -f ../customermanagementapi-svc.yaml `
     -f ../vehiclemanagementapi$meshPostfix.yaml `
     -f ../workshopmanagementapi$meshPostfix.yaml `
-    -f ../webapp$meshPostfix.yaml
+    -f ../webapp$meshPostfix.yaml `
+    -f ../diyservice.yaml
