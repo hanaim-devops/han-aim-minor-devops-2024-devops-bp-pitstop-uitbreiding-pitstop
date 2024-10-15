3d22da33d64fcaad7b0c7dbe98e3ab6cf7556c97
GoobyTheBOI <luuklentjes2002@gmail.com>
2024-10-15 15:41:15 +0200

    #17 Added DBContext for Maintenance History

---
+++ src/MaintenanceHistoryAPI/Migrations/20241015132837_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaintenanceHistoryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MaintenanceHistory",
+                columns: table => new
+                {
+                    LicenseNumber = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    MaintenanceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaintenanceType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaintenanceHistory", x => x.LicenseNumber);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MaintenanceHistory");
+        }
+    }
+}
