01b1c505bed85b246990b7d1ecd77d1fefd068a7
Jelmer Noppert <j.noppert@student.han.nl>
2024-10-16 11:51:10 +0200

    Added minimal logic files for DIY pages

---
+++ src/WebApp/Models/DIYAvond.cs
@@ -0,0 +1,15 @@
+﻿namespace Pitstop.WebApp.Models
+{
+    public class DIYAvond
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Location { get; set; }
+        public string MaxRegistrations { get; set; }
+        public string Registrations { get; set; }   
+    }
+}
