c4f19b3b6acff66688abbbb591b9b29fbaa5d50a
Luuk <47282593+luukjp@users.noreply.github.com>
2024-10-17 22:37:40 +0200

    Add vehiclemanagementapi pipeline

--- tekton/apply-all.sh
+++ tekton/apply-all.sh
@@ -20,7 +20,8 @@ kubectl apply -f ./pipeline/build-and-deploy.yaml
 #kubectl delete pipelinerun notificationservice-build-run
 #kubectl delete pipelinerun invoiceservice-build-run
 #kubectl delete pipelinerun auditlogservice-build-run
-kubectl delete pipelinerun workshopmanagementapi-build-run
+#kubectl delete pipelinerun workshopmanagementapi-build-run
+kubectl delete pipelinerun vehiclemanagementapi-build-run
 #kubectl apply -f ./pipeline-run/dotnet-aspnet-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/dotnet-runtime-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/dotnet-sdk-base-build-run.yaml
@@ -31,7 +32,8 @@ kubectl delete pipelinerun workshopmanagementapi-build-run
 #kubectl apply -f ./pipeline-run/notificationservice-build-run.yaml
 #kubectl apply -f ./pipeline-run/invoiceservice-build-run.yaml
 #kubectl apply -f ./pipeline-run/auditlogservice-build-run.yaml
-kubectl apply -f ./pipeline-run/workshopmanagementapi-build-run.yaml
+#kubectl apply -f ./pipeline-run/workshopmanagementapi-build-run.yaml
+kubectl apply -f ./pipeline-run/vehiclemanagementapi-build-run.yaml
 
 #tkn pipelinerun logs dotnet-aspnet-base-build-run --follow
 #tkn pipelinerun describe dotnet-aspnet-base-build-run
@@ -63,5 +65,8 @@ kubectl apply -f ./pipeline-run/workshopmanagementapi-build-run.yaml
 #tkn pipelinerun logs auditlogservice-build-run --follow
 #tkn pipelinerun describe auditlogservice-build-run
 
-tkn pipelinerun logs workshopmanagementapi-build-run --follow
-tkn pipelinerun describe workshopmanagementapi-build-run
+#tkn pipelinerun logs workshopmanagementapi-build-run --follow
+#tkn pipelinerun describe workshopmanagementapi-build-run
+
+tkn pipelinerun logs vehiclemanagementapi-build-run --follow
+tkn pipelinerun describe vehiclemanagementapi-build-run
