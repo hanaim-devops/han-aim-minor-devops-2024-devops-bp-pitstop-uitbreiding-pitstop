aee8174de95ace10d8f454d8e785e0ae2def3665
Luuk <47282593+luukjp@users.noreply.github.com>
2024-10-17 22:24:33 +0200

    Add workshopmanagementeventhandler pipeline

--- tekton/apply-all.sh
+++ tekton/apply-all.sh
@@ -14,24 +14,29 @@ kubectl apply -f ./pipeline/build-and-deploy.yaml
 #kubectl delete pipelinerun dotnet-runtime-base-build-run
 #kubectl delete pipelinerun dotnet-sdk-base-build-run
 #kubectl delete pipelinerun customermanagementapi-build-run
-kubectl delete pipelinerun webapp-build-run
+#kubectl delete pipelinerun webapp-build-run
+kubectl delete pipelinerun workshopmanagementeventhandler-build-run
 #kubectl apply -f ./pipeline-run/dotnet-aspnet-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/dotnet-runtime-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/dotnet-sdk-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/customermanagementapi-build-run.yaml
-kubectl apply -f ./pipeline-run/webapp-build-run.yaml
+#kubectl apply -f ./pipeline-run/webapp-build-run.yaml
+kubectl apply -f ./pipeline-run/workshopmanagementeventhandler-build-run.yaml
 
-tkn pipelinerun logs dotnet-aspnet-base-build-run --follow
-tkn pipelinerun describe dotnet-aspnet-base-build-run
+#tkn pipelinerun logs dotnet-aspnet-base-build-run --follow
+#tkn pipelinerun describe dotnet-aspnet-base-build-run
 
-tkn pipelinerun logs dotnet-runtime-base-build-run --follow
-tkn pipelinerun describe dotnet-runtime-base-build-run
+#tkn pipelinerun logs dotnet-runtime-base-build-run --follow
+#tkn pipelinerun describe dotnet-runtime-base-build-run
 
-tkn pipelinerun logs dotnet-sdk-base-build-run --follow
-tkn pipelinerun describe dotnet-sdk-base-build-run
+#tkn pipelinerun logs dotnet-sdk-base-build-run --follow
+#tkn pipelinerun describe dotnet-sdk-base-build-run
 
-tkn pipelinerun logs customermanagementapi-build-run --follow
-tkn pipelinerun describe customermanagementapi-build-run
+#tkn pipelinerun logs customermanagementapi-build-run --follow
+#tkn pipelinerun describe customermanagementapi-build-run
 
-tkn pipelinerun logs webapp-build-run --follow
-tkn pipelinerun describe webapp-build-run
+#tkn pipelinerun logs webapp-build-run --follow
+#tkn pipelinerun describe webapp-build-run
+
+tkn pipelinerun logs workshopmanagementeventhandler-build-run --follow
+tkn pipelinerun describe workshopmanagementeventhandler-build-run
