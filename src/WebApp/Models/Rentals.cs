955fd3dac869c3c056e817a2b0bbd82d58a92128
DaanMemelink <memelinkd@gmail.com>
2024-10-17 11:36:44 +0200

    #6 Created (create) rent vehicle pages

---
+++ src/WebApp/Models/Rentals.cs
@@ -0,0 +1,18 @@
+﻿namespace Pitstop.WebApp.Models;
+
+public class Rentals
+{
+    public Guid Id { get; set; }
+
+    [Display(Name = "Vehicle")]
+    public Vehicle Vehicle { get; set; }
+
+    [Display(Name = "Start At")]
+    public DateTime? StartDate { get; set; }
+    
+    [Display(Name = "Ends At")]
+    public DateTime? EndDate { get; set; }
+    
+    [Display(Name = "Renter")]
+    public Customer Customer { get; set; }
+}
