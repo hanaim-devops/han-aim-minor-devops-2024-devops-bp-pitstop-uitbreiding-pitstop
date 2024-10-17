71c1caccea6b5df17f70b0fa3d45bf624b1e3121
Luuk <47282593+luukjp@users.noreply.github.com>
2024-10-17 22:29:17 +0200

    Add notificationservice pipeline

--- tekton/apply-all.sh
+++ tekton/apply-all.sh
@@ -16,14 +16,16 @@ kubectl apply -f ./pipeline/build-and-deploy.yaml
 #kubectl delete pipelinerun customermanagementapi-build-run
 #kubectl delete pipelinerun webapp-build-run
 #kubectl delete pipelinerun workshopmanagementeventhandler-build-run
-kubectl delete pipelinerun workshopmanagementeventhandler-build-run
+#kubectl delete pipelinerun timeservice-build-run
+kubectl delete pipelinerun notificationservice-build-run
 #kubectl apply -f ./pipeline-run/dotnet-aspnet-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/dotnet-runtime-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/dotnet-sdk-base-build-run.yaml
 #kubectl apply -f ./pipeline-run/customermanagementapi-build-run.yaml
 #kubectl apply -f ./pipeline-run/webapp-build-run.yaml
 #kubectl apply -f ./pipeline-run/workshopmanagementeventhandler-build-run.yaml
-kubectl apply -f ./pipeline-run/timeservice-build-run.yaml
+#kubectl apply -f ./pipeline-run/timeservice-build-run.yaml
+kubectl apply -f ./pipeline-run/notificationservice-build-run.yaml
 
 #tkn pipelinerun logs dotnet-aspnet-base-build-run --follow
 #tkn pipelinerun describe dotnet-aspnet-base-build-run
@@ -43,5 +45,8 @@ kubectl apply -f ./pipeline-run/timeservice-build-run.yaml
 #tkn pipelinerun logs workshopmanagementeventhandler-build-run --follow
 #tkn pipelinerun describe workshopmanagementeventhandler-build-run
 
-tkn pipelinerun logs timeservice-build-run --follow
-tkn pipelinerun describe timeservice-build-run
+#tkn pipelinerun logs timeservice-build-run --follow
+#tkn pipelinerun describe timeservice-build-run
+
+tkn pipelinerun logs notificationservice-build-run --follow
+tkn pipelinerun describe notificationservice-build-run
