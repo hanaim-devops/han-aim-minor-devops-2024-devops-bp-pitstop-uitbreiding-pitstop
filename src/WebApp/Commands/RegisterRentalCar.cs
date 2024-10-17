bdcc9e844de26dbd34c34b0e38247ac114d6c5dd
DaanMemelink <memelinkd@gmail.com>
2024-10-17 14:07:21 +0200

    Frontend created

---
+++ src/WebApp/Commands/RegisterRentalCar.cs
@@ -0,0 +1,16 @@
+﻿namespace Pitstop.WebApp.Commands;
+
+public class RegisterRentalCar : Command
+{
+    public readonly string LicenseNumber;
+    public readonly string Brand;
+    public readonly string Type;
+
+    public RegisterRentalCar(Guid messageId, string licenseNumber, string brand, string type) :
+        base(messageId)
+    {
+        LicenseNumber = licenseNumber;
+        Brand = brand;
+        Type = type;
+    }
+}
