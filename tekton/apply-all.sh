4d913cc8ae41672e3fe5afada86d33429de55022
Luuk <47282593+luukjp@users.noreply.github.com>
2024-10-17 21:54:56 +0200

    local debug script

---
+++ tekton/apply-all.sh
@@ -0,0 +1,32 @@
+kubectl delete all --all
+
+kubectl delete task build-and-deploy
+kubectl apply -f ./task/git-clone.yaml
+kubectl apply -f ./task/kaniko.yaml
+
+kubectl delete taskrun build-and-deploy
+kubectl apply -f ./task-run/git-clone-run.yaml
+
+kubectl delete pipeline build-and-deploy
+kubectl apply -f ./pipeline/build-and-deploy.yaml
+
+kubectl delete pipelinerun dotnet-aspnet-base-build-run
+kubectl delete pipelinerun dotnet-runtime-base-build-run
+kubectl delete pipelinerun dotnet-sdk-base-build-run
+kubectl delete pipelinerun customermanagementapi-build-run
+kubectl apply -f ./pipeline-run/dotnet-aspnet-base-build-run.yaml
+kubectl apply -f ./pipeline-run/dotnet-runtime-base-build-run.yaml
+kubectl apply -f ./pipeline-run/dotnet-sdk-base-build-run.yaml
+kubectl apply -f ./pipeline-run/customermanagementapi-build-run.yaml
+
+tkn pipelinerun logs dotnet-aspnet-base-build-run --follow
+tkn pipelinerun describe dotnet-aspnet-base-build-run
+
+tkn pipelinerun logs dotnet-runtime-base-build-run --follow
+tkn pipelinerun describe dotnet-runtime-base-build-run
+
+tkn pipelinerun logs dotnet-sdk-base-build-run --follow
+tkn pipelinerun describe dotnet-sdk-base-build-run
+
+tkn pipelinerun logs customermanagementapi-build-run --follow
+tkn pipelinerun describe customermanagementapi-build-run
